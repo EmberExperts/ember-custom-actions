26df719a2f11df5926c470fcf8e57d2dad6dfea0
Maciej Kwaśniak <kmaciek17@gmail.com>
2017-02-11 21:00:59 +0100

    Add metas

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,9 +4,22 @@
 var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
+  var config = defaults.project.config(EmberAddon.env());
+
   var app = new EmberAddon(defaults, {
     sassOptions: {
       extension: 'sass'
+    },
+    favicons: {
+      faviconsConfig: {
+        appName: 'Ember Custom Actions',
+        appDescription: 'Trigger custom API actions for Ember 2 applications and communicate with non-CRUD API servers',
+        developerName: 'Exelord',
+        developerURL: 'www.macsour.com',
+        background: '#ffffff',
+        path: config.rootURL,  // Path for overriding default icons path. `string`
+        url: 'https://exelord.github.io/ember-custom-actions/images/og-image.jpg'  // Absolute URL for OpenGraph image. `string`
+      }
     }
   });
 
