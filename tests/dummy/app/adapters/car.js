aa16497bf9972f5e51130d1e5e560595b51c4de9
Krzyrok <krzyrok@gmail.com>
2017-10-05 09:08:20 +0200

    used public buildURL from adapter instead of _buildURL

--- tests/dummy/app/adapters/car.js
+++ tests/dummy/app/adapters/car.js
@@ -7,12 +7,12 @@ export default JSONAPIAdapter.extend(AdapterMixin, {
     suffix = suffix || '';
 
     if (requestType === 'clean') {
-      let baseUrl = this._buildURL(modelName, id);
+      let baseUrl = this.buildURL(modelName, id, snapshot, requestType);
       return `${baseUrl}/custom-clean`;
     } else if (requestType === 'fix') {
       return `/custom-cars/${id}/custom-fix${suffix}`;
     } else if (requestType === 'clean-all') {
-      let baseUrl = this._buildURL(modelName, id);
+      let baseUrl = this.buildURL(modelName, id, snapshot, requestType);
       return `${baseUrl}/custom-clean-all`;
     } else if (requestType === 'fix-all') {
       return `/custom-cars/custom-fix-all${suffix}`;
