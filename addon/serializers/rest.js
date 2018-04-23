d7bdffe82e299d48b03d385f8a62df7c771a3e0b
Maciej Kwaśniak <kmaciek17@gmail.com>
2018-04-23 20:45:23 +0200

    Update deprecations

--- addon/serializers/rest.js
+++ addon/serializers/rest.js
@@ -8,9 +8,9 @@ export default RESTSerializer.extend({
   init() {
     this._super(...arguments);
 
-    deprecate('Using ember-custom-actions `RestSerializer` is no longer required and this class will be removed in the next version.', false, {
+    deprecate('Using ember-custom-actions `RestSerializer` is no longer required and this class will be removed.', false, {
       id: 'ember-custom-actions.deprecate-jsonapi-serializer',
-      until: '2.2.0'
+      until: '3.0.0'
     });
   },
 });
