9c78f774e1740176edf6862308666ea699fac17f
Gabor Babicz <gabor.babicz@gmail.com>
2018-08-22 09:13:10 +0200

    Substitute jQuery.param function

---
+++ addon/utils/param.js
@@ -0,0 +1,41 @@
+/* eslint-disable */
+// https://github.com/knowledgecode/jquery-param/blob/master/jquery-param.js
+export default function param(a) {
+  var s = [];
+  var add = function (k, v) {
+    v = typeof v === 'function' ? v() : v;
+    v = v === null ? '' : v === undefined ? '' : v;
+    s[s.length] = encodeURIComponent(k) + '=' + encodeURIComponent(v);
+  };
+  var buildParams = function (prefix, obj) {
+    var i, len, key;
+
+    if (prefix) {
+      if (Array.isArray(obj)) {
+        for (i = 0, len = obj.length; i < len; i++) {
+          buildParams(
+            prefix + '[' + (typeof obj[i] === 'object' && obj[i] ? i : '') + ']',
+            obj[i]
+            );
+        }
+      } else if (String(obj) === '[object Object]') {
+        for (key in obj) {
+          buildParams(prefix + '[' + key + ']', obj[key]);
+        }
+      } else {
+        add(prefix, obj);
+      }
+    } else if (Array.isArray(obj)) {
+      for (i = 0, len = obj.length; i < len; i++) {
+        add(obj[i].name, obj[i].value);
+      }
+    } else {
+      for (key in obj) {
+        buildParams(key, obj[key]);
+      }
+    }
+    return s;
+  };
+
+  return buildParams('', a).join('&');
+}
