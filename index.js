3864bfe29404b7321426bce2d1dd3119d4c0f055
Maciej Kwaśniak <kmaciek17@gmail.com>
2018-09-07 13:27:21 -0700

    Remove lodash

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-custom-actions'
+  name: 'ember-custom-actions',
+
+  importTransforms: require('ember-cli-cjs-transform').importTransforms,
+
+  included() {
+    this._super.included.apply(this, arguments);
+
+    this.import('node_modules/lodash.merge/index.js', {
+      using: [
+        { transformation: 'cjs', as: 'lodash.merge' }
+      ]
+    });
+  },
 };
